c1ba108fa0f66003fa1daaf1ab9cbfa662909c30
luisro369 <00005513@uca.edu.sv>
2018-06-03 20:54:38 -0600

    mi primer commit en brnach

--- app/src/main/java/com/luisro00005513/pruebaretrofit/MainActivity.java
+++ app/src/main/java/com/luisro00005513/pruebaretrofit/MainActivity.java
@@ -78,4 +78,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    void algo(){
+
+
+    }
+
 }
---
+++ app/src/main/java/com/luisro00005513/pruebaretrofit/claseABorrar.java
@@ -0,0 +1,8 @@
+package com.luisro00005513.pruebaretrofit;
+
+/**
+ * Created by luisro on 6/3/18.
+ */
+
+public class claseABorrar {
+}
